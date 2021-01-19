2d093ac28c6fcfbfbd5031a3dd503b59de139845
Hamdani Azzah <53028113+hamdaniazzah@users.noreply.github.com>
2021-01-19 10:51:52 +0700

    Update and rename a_2.js to a_3.js

--- redskip/top3/a_3.js
+++ redskip/top3/a_3.js
@@ -1,5 +1,7 @@
+var date=new Date,thn=date.getFullYear(),bln=date.getMonth(),tgl=date.getDate();switch(bln){case 0:bln="02";case 1:bln="03";case 2:bln="04";case 3:bln="05";case 4:bln="06";case 5:bln="07";case 6:bln="08";case 7:bln="09";case 8:bln="10";case 9:bln="11";case 10:bln="12";case 11:bln="01"}
+
 $(function(){
-  if ((location.search.indexOf('') === -1) || (location.search.indexOf('?a=') !== -1)) {
+  if ((location.search.indexOf("") === -1) || (location.search.indexOf("?utm-source=link3&id="+thn+bln+tgl+"&alt=") !== -1)) {
 /* START */
 var l3id = window.location.search.substring(34);history.pushState('', document.title, window.location.pathname);
 $(".link3_post").hide();
